712aae0e12c3d63633b6bc1478134b73afe21c0a
Raphael Cohn <raphael.cohn@stormmq.com>
2020-05-09 08:42:57 +0100

    Tweaks

--- workspace/linux-support/src/io_uring/CompletionQueueEntry.rs
+++ workspace/linux-support/src/io_uring/CompletionQueueEntry.rs
@@ -3,5 +3,40 @@
 
 
 /// Completion queue entry, `CQE`.
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct CompletionQueueEntry<'a>(&'a io_uring_cqe);
+
+impl CompletionQueueEntry<'_>
+{
+	/// User data.
+	///
+	/// Use this to link back to the file descriptor, any state associated with it and any buffers used in the original submission queue entry that can now be freed.
+	///
+	/// Some buffers can be freed or reused once the submission queue entry has been removed.
+	#[inline(always)]
+	pub fn user_data(self) -> u64
+	{
+		self.0.user_data
+	}
+
+	/// Overlaps with `buffer_identifier`, below.
+	///
+	/// If negative, and between -1 and -4095 inclusive, should represent a Linux error code negated such as `-EINVAL`.
+	#[inline(always)]
+	pub fn result_or_error(self) -> i32
+	{
+		self.0.res
+	}
+
+	#[inline(always)]
+	pub fn has_buffer_identifier(self) -> bool
+	{
+		self.0.has_buffer_identifier()
+	}
+
+	#[inline(always)]
+	pub fn buffer_identifier(self) -> u16
+	{
+		self.0.buffer_identifier()
+	}
+}
