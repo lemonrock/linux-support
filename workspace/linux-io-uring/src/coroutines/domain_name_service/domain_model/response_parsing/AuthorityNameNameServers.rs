3d3ac715a2d090e69d29ed562ac39a9ef1414063
Raphael Cohn <raphael.cohn@stormmq.com>
2020-10-29 14:25:42 +0000

    latest

--- workspace/linux-io-uring/src/coroutines/domain_name_service/domain_model/response_parsing/AuthorityNameNameServers.rs
+++ workspace/linux-io-uring/src/coroutines/domain_name_service/domain_model/response_parsing/AuthorityNameNameServers.rs
@@ -2,6 +2,7 @@
 // Copyright © 2020 The developers of linux-support. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/linux-support/master/COPYRIGHT.
 
 
+#[derive(Debug, Clone)]
 pub(crate) struct AuthorityNameNameServers<'cache>
 {
 	pub(crate) authority_name: CaseFoldedName<'cache>,
